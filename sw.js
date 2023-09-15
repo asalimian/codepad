e17e683a676bf8eacb1c005b03e53a54d37f89c6
Alexander Salimian <alexsalimian@gmail.com>
2023-09-14 23:21:04 -0400

    Fixed wrap data loss. Add auto resize, mobile view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'codepad-v1.2.1';
+const CACHE_NAME = 'codepad-v1.2.2';
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
