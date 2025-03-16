ddb7a747f4fb2a2bb658034658d13d14503129ab
Alexander Salimian <alexsalimian@gmail.com>
2025-03-16 15:23:33 -0400

    Updated sw.js version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "codepad-v1.3.0";
+const CACHE_NAME = "codepad-v1.4.0";
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener("install", (event) => {
