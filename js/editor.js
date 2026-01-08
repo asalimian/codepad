e8a62872e4472f79d4d112caf445fe84ae602f27
Alexander Salimian <alexsalimian@gmail.com>
2026-01-08 22:02:52 +0000

    Replaced hosted monaco with unpkg

--- js/editor.js
+++ js/editor.js
@@ -14,7 +14,7 @@ class EditorManager {
 
   async initMonaco() {
     require.config({
-      paths: { vs: "/codepad/node_modules/monaco-editor/min/vs" },
+      paths: { vs: "https://unpkg.com/monaco-editor@latest/min/vs" },
     });
 
     await require(["vs/editor/editor.main"], () => {
